a6660917fb7118aded1daab780c5da909eaceff0
Willem Leuverink <willem@leuver.ink>
2023-09-19 23:43:23 +0200

    publish additional config files

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -29,14 +29,18 @@ class ServiceProvider extends BaseServiceProvider
         ], 'janitor-config');
 
         $this->publishes([
+            __DIR__ . '/../resources/config/.php-cs-fixer.dist.php' => base_path('.php-cs-fixer.dist.php'),
             __DIR__ . '/../resources/config/duster-with-custom-configs.json' => base_path('duster.json'),
+            __DIR__ . '/../resources/config/.phpcs.xml.dist' => base_path('.phpcs.xml.dist'),
             __DIR__ . '/../resources/config/phpstan.neon' => base_path('phpstan.neon'),
+            __DIR__ . '/../resources/config/tlint.json' => base_path('tlint.json'),
+            __DIR__ . '/../resources/config/pint.json' => base_path('pint.json'),
         ], 'janitor-3rd-party-configs');
 
         $this->publishes([
+            __DIR__ . '/../resources/workflows/static-analysis.yml' => base_path('static-analysis.yml'),
             __DIR__ . '/../resources/workflows/duster-fix.yml' => base_path('duster-fix.yml'),
             __DIR__ . '/../resources/workflows/pest-tests.yml' => base_path('pest-tests.yml'),
-            __DIR__ . '/../resources/workflows/static-analysis.yml' => base_path('static-analysis.yml'),
         ], 'janitor-github-actions');
     }
 }

ebbee4ea84bab023966d9d87bf5485ca062c1b20
Willem Leuverink <willem@leuver.ink>
2023-09-27 20:20:06 +0200

    add editorconfig

--- src/Integrations/EditorDefaults/EditorDefaults.php
+++ src/Integrations/EditorDefaults/EditorDefaults.php
@@ -7,17 +7,21 @@ use Gedachtegoed\Janitor\Core\Builder;
 class EditorDefaults extends Builder
 {
     public function __invoke() {
-        $this->provideVscodeWorkspaceConfig([
-            'files.associations' => (object) [
-                '.php_cs' => 'php',
-                '.php_cs.dist' => 'php',
-                '.env*' => 'dotenv'
-            ],
-            'emmet.includeLanguages' => (object) [
-                'blade' => 'html',
-                'vue-html' => 'html',
-                'vue' => 'html'
-            ],
-        ]);
+        $this
+            ->publishesConfigs([
+                '.editorconfig' => base_path('.editorconfig')
+            ])
+            ->provideVscodeWorkspaceConfig([
+                'files.associations' => (object) [
+                    '.php_cs' => 'php',
+                    '.php_cs.dist' => 'php',
+                    '.env*' => 'dotenv'
+                ],
+                'emmet.includeLanguages' => (object) [
+                    'blade' => 'html',
+                    'vue-html' => 'html',
+                    'vue' => 'html'
+                ],
+            ]);
     }
 }

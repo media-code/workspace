21b1ec8bc2f079bea1ecca26a4f487917f68c3fd
Willem Leuverink <willem@leuver.ink>
2023-10-03 21:09:19 +0200

    fix typo

--- tests/Unit/ArchitectureTest.php
+++ tests/Unit/ArchitectureTest.php
@@ -7,6 +7,6 @@ test("doesn't use final classes")
     ->expect('Gedachtegoed\\Workspace\\Core')
     ->classes()->not->toBeFinal();
 
-test('Concerns namespace only contains traits')
+test('Concerns namespaces only contains traits')
     ->expect('Core\\Concerns')->toBeTraits()
     ->expect('Commands\\Concerns')->toBeTraits();
